47ca9906e0a7da1bc21c9007a601adc23f0b3a9b
haidyeed <haidyeed@gmail.com>
2024-01-11 15:35:27 +0200

    filling database

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -3,9 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Admin extends Authenticatable
 {
+    use HasFactory;
     protected $guard = 'admin';
 
     /**
